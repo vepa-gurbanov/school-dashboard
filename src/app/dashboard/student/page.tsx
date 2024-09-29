1768065ebc216113cc5dd3d128afb5fe606ca940
vepa-gurbanov <tazesalgy@gmail.com>
2024-09-29 23:10:01 +0500

    v1.0.2.1 - student page process

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -1,7 +1,24 @@
+import Announcements from "@/app/components/Announcements";
+import BigCalendar from "@/app/components/BigCalendar";
+import EventCalendar from "@/app/components/EventCalendar";
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
 
-export default StudentPage
+export default StudentPage;
